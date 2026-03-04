69b54b7bf30a9cc97e17e978b5d931d3909ea919
himanshiiv <164589950+himanshiiv@users.noreply.github.com>
2026-03-04 04:40:02 -0500

    Update userstory1(eecs2311).sql
    Add PostgreSQL schema for user story 1 and 2

--- userstory1(eecs2311).sql
+++ userstory1(eecs2311).sql
@@ -31,6 +31,23 @@ CREATE TABLE assessments (
         -- If a course is deleted, its assessments are deleted automatically
 );
 
+-- SCORES TABLE
+-- Stores actual grades entered by student
+
+CREATE TABLE scores (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+
+    assessment_id UUID UNIQUE NOT NULL,
+
+    score DECIMAL(5,2) CHECK (score >= 0 AND score <= 100),
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (assessment_id)
+        REFERENCES assessments(id)
+        ON DELETE CASCADE
+);
+
 
 -- RULES TABLE
 -- Stores grading rules (best-of, drop lowest, etc.)
@@ -48,6 +65,44 @@ CREATE TABLE rules (
         ON DELETE CASCADE
 );
 
+-- SCENARIOS TABLE
+-- Stores What-If scenarios
+
+CREATE TABLE scenarios (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+
+    course_id UUID NOT NULL,
+
+    name VARCHAR(255) NOT NULL,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (course_id)
+        REFERENCES courses(id)
+        ON DELETE CASCADE
+);
+
+-- SCENARIO SCORES TABLE
+-- Stores hypothetical scores inside a scenario
+
+CREATE TABLE scenario_scores (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+
+    scenario_id UUID NOT NULL,
+    assessment_id UUID NOT NULL,
+
+    simulated_score DECIMAL(5,2),
+
+    FOREIGN KEY (scenario_id)
+        REFERENCES scenarios(id)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY (assessment_id)
+        REFERENCES assessments(id)
+        ON DELETE CASCADE,
+
+    UNIQUE (scenario_id, assessment_id)
+);
 
 --  INDEXES
 -- Makes course lookups faster
@@ -56,3 +111,12 @@ ON assessments(course_id);
 
 CREATE INDEX idx_rules_course_id
 ON rules(course_id);
+
+-- INDEXES FOR PERFORMANCE
+
+CREATE INDEX idx_scores_assessment_id
+ON scores(assessment_id);
+
+CREATE INDEX idx_scenarios_course_id
+ON scenarios(course_id);
+
